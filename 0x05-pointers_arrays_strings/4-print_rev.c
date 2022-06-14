02c82de0258fce5de0dc17cd41d3cb8942e727ba
LeratoNtini <lslntini@hotmail.com>
2022-06-14 02:00:30 -0700

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,24 +1,22 @@
 #include "main.h"
+
 /**
-  * print_rev - Returns the length of a string.
-  * @s: char pointer.
-  * Return: length of a string.
-  */
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
 void print_rev(char *s)
 {
-	int count;
+	int count = 0;
 
-	while (*s != 0)
+	while (count >= 0)
 	{
+		if (s[count] == '\0')
+			break;
 		count++;
-		s++;
 	}
-	s--;
-	while (count - 1 >= 0)
-	{
-		_putchar(*s);
-		count--;
-		s--;
-	}
-	_putchar(10);
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
