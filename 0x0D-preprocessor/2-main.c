7728a0d5478d73d4292fd3fc541090a342206d35
LeratoNtini <lslntini@hotmail.com>
2022-07-15 04:08:12 -0700

    com

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,3 +1,16 @@
-#ifndef PI
-#define PI 3.14159265359
-#endif
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
+
